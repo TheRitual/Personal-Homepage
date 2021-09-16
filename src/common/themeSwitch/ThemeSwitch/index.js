ddae3f6056a57853bbb1aef26e485bfdb5a83bd5
Ritual <dev@theritual.eu>
2021-09-16 16:50:56 +0200

    created ThemeSwitch component

---
+++ src/common/themeSwitch/ThemeSwitch/index.js
@@ -0,0 +1,12 @@
+const ThemeSwitch = ({theme}) => {
+    return (
+        <div>
+            <span>
+                Theme: {theme.colors.shadeName}
+            </span>
+            <button />
+        </div>
+    );
+}
+
+export default ThemeSwitch;
