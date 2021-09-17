a7e5244cdae49686b0fee811ef8c4327af1593e3
Ritual <dev@theritual.eu>
2021-09-17 14:22:57 +0200

    created components for all icons

--- src/features/personalHomepage/PersonalHomepage/icons/index.js
+++ src/features/personalHomepage/PersonalHomepage/icons/index.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import { ReactComponent as BulletIcon } from "../icons/bullet.svg";
 import { ReactComponent as Envelope } from "../icons/envelope.svg";
+import { ReactComponent as SocialFB } from "../icons/social_fb.svg";
+import { ReactComponent as SocialGH } from "../icons/social_gh.svg";
+import { ReactComponent as SocialInsta } from "../icons/social_insta.svg";
+import { ReactComponent as SocialLI } from "../icons/social_lin.svg";
+
+
 
 export const Bullet = styled(BulletIcon)`
     height: 12px;
@@ -15,3 +21,32 @@ export const EnvelopeIcon = styled(Envelope)`
     margin: 0 18px 0 2px;
 `;
 
+export const IconFB = styled(SocialFB)`
+    height: 48px;
+    width: 48px;
+    color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const IconGH = styled(SocialGH)`
+    height: 48px;
+    width: 48px;
+    color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const IconInsta = styled(SocialInsta)`
+    height: 48px;
+    width: 48px;
+    color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const IconLI = styled(SocialLI)`
+    height: 48px;
+    width: 48px;
+    color: ${({ theme }) => theme.colors.textColor};
+`;
+
+export const IconGHPrimary = styled(SocialGH)`
+    height: 40px;
+    width: 40px;
+    color: ${({ theme }) => theme.colors.primary};
+`;
