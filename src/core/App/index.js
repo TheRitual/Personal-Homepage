76909f0d77779b83900748664f3f74a00dfae0a0
Ritual <dev@theritual.eu>
2021-09-19 23:36:56 +0200

    moved Global Styles to App component directory

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import PersonalHomepage from "../../features/personalHomepage/PersonalHomepage";
 import themeConstructor from "../../utils/themes";
-import { GlobalStyle } from "../globalStyles";
+import { GlobalStyle } from "./globalStyles";
 
 const selectedTheme = themeConstructor("light", "blue");
 
