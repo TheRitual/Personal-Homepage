cf050eaddcb3e89baaf9598f0a50663336de18af
Ritual <dev@theritual.eu>
2021-09-19 23:22:30 +0200

    installed redux, redux toolkit and redux-saga

--- src/features/personalHomepage/PersonalHomepage/sections/Footer/SocialIcons/index.js
+++ src/features/personalHomepage/PersonalHomepage/sections/Footer/SocialIcons/index.js
@@ -5,10 +5,10 @@ import { IconsWrapper, SocialLink } from "./styled";
 const SocialIcons = () => {
     return (
         <IconsWrapper>
-            <SocialLink href="https://github.com/TheRitual"> <IconGH /> </SocialLink>
-            <SocialLink href="https://github.com/TheRitual"> <IconFB /> </SocialLink>
-            <SocialLink href="https://github.com/TheRitual"> <IconLI /> </SocialLink>
-            <SocialLink href="https://github.com/TheRitual"> <IconInsta /> </SocialLink>
+            <SocialLink href="https://github.com/TheRitual" target="_blank"> <IconGH /> </SocialLink>
+            <SocialLink href="https://www.facebook.com/ZielonyMowi" target="_blank"> <IconFB /> </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/marcin-kawczy%C5%84ski-122734105/" target="_blank"> <IconLI /> </SocialLink>
+            <SocialLink href="https://www.instagram.com/mgik_ritual/" target="_blank"> <IconInsta /> </SocialLink>
         </IconsWrapper>
     );
 }
