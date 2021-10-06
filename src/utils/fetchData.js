ac29957f3c60d0c232db35f8f32bfa2d0dfc8bbe
Ritual <dev@theritual.eu>
2021-10-06 19:20:57 +0200

    corrected link

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchRepos = async (org) => {
   try {
-      const response = await axios.get(`https://api.github.com/repos/${org}`);
+      const response = await axios.get(`https://api.github.com/users/${org}/repos`);
       return response;
     } catch (error) {
       console.error(error);
