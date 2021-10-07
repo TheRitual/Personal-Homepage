1851835f80e1392594b51033320d34ca4b935219
Ritual <dev@theritual.eu>
2021-10-07 03:36:31 +0200

    changed protocol to https

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchRepos = async () => {
   try {
-    const response = await axios.get(`http://dev.theritual.eu/repos/`);
+    const response = await axios.get(`https://dev.theritual.eu/repos/`);
     return response.data;
   } catch (error) {
     console.error(error);
